8bb93188b37e9f4c52e76a9b84dd30b014d6e3fc
wujueying <652178160@qq.com>
2021-10-25 22:29:39 +0800

    补充

---
+++ 1.js
@@ -0,0 +1,22 @@
+const array = [1,2,3,[4,5,[2]]];
+const aa = (arr) => {
+  let newArray = [];
+  const arrMap = (arr)=>{
+    arr.map((item,index)=>{
+        if(typeof item === 'number'){
+          newArray.push(item);
+        }
+    })
+  }
+  arr.map((item,index)=>{
+    if(typeof item === 'number'){
+      newArray.push(item);
+    }else{
+        arrMap(item);
+    }
+})
+
+return newArray;
+}
+
+console.log(aa(array)) ;
